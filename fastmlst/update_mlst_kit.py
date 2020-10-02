862d546ba600439893e6602fa974489eb328f3e8
Enzo <EnzoAndree@users.noreply.github.com>
2020-10-02 18:46:05 -0300

    ;ajor bug fixed
    It is a shame that I have never tested other schemes (hello Cjejuni :anger: ). FastMLST now handles well all (I hope) the 150 schemes available in PubMLST.

--- fastmlst/update_mlst_kit.py
+++ fastmlst/update_mlst_kit.py
@@ -29,6 +29,68 @@ def load_obj(name):
     with open(name, 'rb') as f:
         return load(f)
 
+def best_guess_codename(text):
+    # i hope that this is correct
+    text = text.replace('.', '').strip()
+    text = text.replace('Candidatus ', '')
+    text = text.replace('/', '.')
+    text = text.replace('(', '')
+    text = text.replace(')', '')
+    # no more bugs plz!
+    if len(text.split(' ')) == 1:
+        # is a genus scheme
+        if '#' in text:
+            # sh#t, there is multiples schemes for a sigle specie :(
+            components = text.split('#')
+            genus = components[0]
+            schemenumber = components[-1]
+            codename = f'{genus}#{schemenumber}'
+        else:
+            codename = f'{text}'
+    elif 'spp' in text:
+        # is a genus scheme
+        if '#' in text:
+            # sh#t, there is multiples schemes for a sigle specie :(
+            components = text.split(' ')
+            genus = components[0]
+            schemenumber = text.split('#')[-1]
+            codename = f'{genus}#{schemenumber}'
+        else:
+            components = text.split(' ')
+            genus = components[0]
+            codename = f'{genus}'
+    elif len(text.split(' ')) == 2:
+        # is a clasic genus_species like scheme
+        if '#' in text:
+            # sh#t, there is multiples schemes for a sigle specie :(
+            components = text.split(' ')
+            genus = components[0][0].upper()
+            specie = components[1].split('#')[0]
+            schemenumber = text.split('#')[-1]
+            codename = f'{genus}{specie}#{schemenumber}'
+        else:
+            components = text.split(' ')
+            genus = components[0][0].upper()
+            specie = components[1]
+            codename = f'{genus}{specie}'
+    elif len(text.split(' ')) > 2:
+        # is a genus_species_etc... like scheme
+        if '#' in text:
+            # sh#t, there is multiples schemes for a sigle specie :(
+            components = text.split(' ')
+            genus = components[0][0].upper()
+            specie = components[1].split('#')[0]
+            extra = '_'.join(components[2:])
+            schemenumber = text.split('#')[-1]
+            codename = f'{genus}{specie}_{extra}#{schemenumber}'
+        else:
+            components = text.split(' ')
+            genus = components[0][0].upper()
+            specie = components[1]
+            extra = '_'.join(components[2:])
+            codename = f'{genus}{specie}_{extra}'
+    return codename.strip()
+
 
 def parseXML(xml):
     tree = ET.parse(xml)
@@ -38,19 +100,23 @@ def parseXML(xml):
         data = []
         for child in parent.iter('url'):
             data.append(child.text)
-        codename = data[1].strip('/').split('/')[-4].split('_')[1]
+        # BUG solved!, this is not the scheme code calculate it using best_guess_codename
+        # codename = '_'.join(data[1].strip('/').split('/')[-4].split('_')[1:-1])
+        codename = best_guess_codename(parent.text)
         species[codename] = data[1:]
     return species
 
 
-def download_fasta(files):
-    codename = files[0].strip('/').split('/')[-4].split('_')[1]
+def download_fasta(items):
+    codename = items[0]
+    files = items[1]
     outdir = Path(str(pathdb) + '/schemes' + '/' + codename)
     outdir.mkdir(exist_ok=True, parents=True)
     for file in files:
         if 'profiles_csv' in file:
+            out_filename = codename
             urlretrieve(file, str(outdir) + '/' +
-                        file.strip('/').split('/')[-4].split('_')[1] + '.txt')
+                        out_filename + '.txt')
         else:
             urlretrieve(file, str(outdir) + '/' +
                         file.strip('/').split('/')[-2] + '.tfa')
@@ -67,8 +133,8 @@ def update_mlstdb(threads):
     logger.info('Starting download of all schemes')
     t = ThreadPool(threads)
     genome_mlst = []
-    for result in tqdm(t.imap_unordered(download_fasta, datadb.values()),
-                       total=len(datadb.values()),
+    for result in tqdm(t.imap_unordered(download_fasta, datadb.items()),
+                       total=len(datadb.items()),
                        desc='Downloading Schemes using {} threads'.
                        format(threads), unit='Schemes', leave=True):
         genome_mlst.append(result)
@@ -96,7 +162,7 @@ def update_mlstdb(threads):
     blastdb_cmd = 'makeblastdb -hash_index -in {0} -dbtype nucl -title \
                    "PubMLST_{1}" -parse_seqids'
     blastdb_cmd = blastdb_cmd.format(str(pathdb) + '/' +
-                                     outfna, date.today().strftime("%d%m%y"))
+                                     outfna, date.today().strftime('%d%m%y'))
     DB_process = Popen(blastdb_cmd, shell=True, stdin=PIPE, stdout=PIPE,
                        stderr=PIPE)
     DB_process.wait()
